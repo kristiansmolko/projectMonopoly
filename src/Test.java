700ae92a3c2bf873d098eec634994c3ac7f4448e
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-11-28 12:24:27 +0100

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,284 @@
+import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Random;
+
+
+public class Test extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+        BorderPane border = new BorderPane();
+        Group center = new Group();
+        Rectangle rect = makeDice();
+        int x = 250;
+        //moves
+        TranslateTransition back = moveBack(rect, x);
+        ParallelTransition par = parallel(rect, x);
+        TranslateTransition translateFigure = new TranslateTransition();
+        //dots in dice
+        Circle c1 = get1(rect, x);
+        Circle[] set2 = get2(rect, x);
+        Circle[] set3 = get3(rect, x);
+        Circle[] set4 = get4(rect, x);
+        Circle[] set5 = get5(rect, x);
+        Circle[] set6 = get6(rect, x);
+        //figure
+        ImageView figureView = figure();
+        //mouse click action
+        EventHandler<MouseEvent> mouse = mouseEvent -> {
+            if (mouseEvent.getSource() == rect){
+                par.play();
+            }
+        };
+        //when dice throw is completed
+        par.setOnFinished(actionEvent -> {
+            int finalRn = randomNum();
+            if (finalRn == 1){
+                root.getChildren().add(c1);
+            }
+            else if (finalRn == 2)
+                for (Circle setC : set2)
+                    root.getChildren().addAll(setC);
+            else if (finalRn == 3)
+                for (Circle setC : set3)
+                    root.getChildren().addAll(setC);
+            else if (finalRn == 4)
+                for (Circle setC : set4)
+                    root.getChildren().addAll(setC);
+            else if (finalRn == 5)
+                for (Circle setC : set5)
+                    root.getChildren().addAll(setC);
+            else if (finalRn == 6)
+                for (Circle setC : set6)
+                    root.getChildren().addAll(setC);
+
+            translateFigure.setByX(-(finalRn*90)+50);
+            translateFigure.setDuration(Duration.millis(1000));
+            translateFigure.setNode(figureView);
+            translateFigure.play();
+            actionEvent.consume();
+        });
+        //when move with figure is completed
+        translateFigure.setOnFinished(actionEvent -> {
+            back.play();
+            root.getChildren().removeAll(set2);
+            root.getChildren().removeAll(set3);
+            root.getChildren().removeAll(set4);
+            root.getChildren().removeAll(set5);
+            root.getChildren().removeAll(set6);
+            root.getChildren().removeAll(c1);
+        });
+
+        //actions
+        rect.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        root.getChildren().addAll(rect);
+
+        //board
+        Label lbl = makeBoard();
+
+        //layout
+        //center of game = board and figure
+        // + later thing
+        center.getChildren().addAll(lbl,figureView);
+
+        //game setup
+        border.setCenter(center);
+        border.setLeft(root);
+
+        Scene scene = new Scene(border, 1200,800);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static int randomNum(){
+        Random rnd = new Random();
+        return (rnd.nextInt(6)+1);
+    }
+
+    private static Circle get1(Rectangle rect, int x){
+        Circle c = new Circle();
+        c.setCenterX(rect.getX() + x + (rect.getWidth()/2));
+        c.setCenterY(rect.getY() + (rect.getHeight()/2));
+        c.setRadius(8);
+        c.setFill(Color.BLACK);
+        return c;
+    }
+
+    private static Circle[] get2(Rectangle rect, int x){
+        Circle c1 = new Circle();
+        Circle c2 = new Circle();
+        c1.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 10);
+        c1.setCenterY(rect.getY() + (rect.getHeight()/2) - 10);
+        c1.setRadius(8);
+        c1.setFill(Color.BLACK);
+        c2.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 10);
+        c2.setCenterY(rect.getY() + (rect.getHeight()/2) + 10);
+        c2.setRadius(8);
+        c2.setFill(Color.BLACK);
+        return new Circle[] {c1,c2};
+    }
+
+    private Circle[] get3(Rectangle rect, int x) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle();
+        Circle c3 = new Circle();
+        Circle[] set = new Circle[] {c1,c2,c3};
+        c1.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 15);
+        c1.setCenterY(rect.getY() + (rect.getHeight()/2) - 15);
+        c2.setCenterX(rect.getX() + x + (rect.getWidth()/2));
+        c2.setCenterY(rect.getY() + (rect.getHeight()/2));
+        c3.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 15);
+        c3.setCenterY(rect.getY() + (rect.getHeight()/2) + 15);
+        for (Circle c : set){
+            c.setFill(Color.BLACK);
+            c.setRadius(6);
+        }
+        return set;
+    }
+
+    private Circle[] get4(Rectangle rect, int x) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle();
+        Circle c3 = new Circle();
+        Circle c4 = new Circle();
+        Circle[] set = new Circle[] {c1,c2,c3,c4};
+        c1.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 13);
+        c1.setCenterY(rect.getY() + (rect.getHeight()/2) - 13);
+        c2.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 13);
+        c2.setCenterY(rect.getY() + (rect.getHeight()/2) + 13);
+        c3.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 13);
+        c3.setCenterY(rect.getY() + (rect.getHeight()/2) + 13);
+        c4.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 13);
+        c4.setCenterY(rect.getY() + (rect.getHeight()/2) - 13);
+        for (Circle c : set){
+            c.setFill(Color.BLACK);
+            c.setRadius(6);
+        }
+        return set;
+    }
+
+    private Circle[] get5(Rectangle rect, int x) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle();
+        Circle c3 = new Circle();
+        Circle c4 = new Circle();
+        Circle c5 = new Circle();
+        Circle[] set = new Circle[] {c1,c2,c3,c4,c5};
+        c1.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 14);
+        c1.setCenterY(rect.getY() + (rect.getHeight()/2) - 14);
+        c2.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 14);
+        c2.setCenterY(rect.getY() + (rect.getHeight()/2) + 14);
+        c3.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 14);
+        c3.setCenterY(rect.getY() + (rect.getHeight()/2) + 14);
+        c4.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 14);
+        c4.setCenterY(rect.getY() + (rect.getHeight()/2) - 14);
+        c5.setCenterX(rect.getX() + x + (rect.getWidth()/2));
+        c5.setCenterY(rect.getY() + (rect.getHeight()/2));
+        for (Circle c : set){
+            c.setFill(Color.BLACK);
+            c.setRadius(6);
+        }
+        return set;
+    }
+
+    private Circle[] get6(Rectangle rect, int x){
+        Circle c1 = new Circle();
+        Circle c2 = new Circle();
+        Circle c3 = new Circle();
+        Circle c4 = new Circle();
+        Circle c5 = new Circle();
+        Circle c6 = new Circle();
+        Circle[] set = new Circle[] {c1,c2,c3,c4,c5,c6};
+        c1.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 15);
+        c1.setCenterY(rect.getY() + (rect.getHeight()/2) - 15);
+        c2.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 15);
+        c2.setCenterY(rect.getY() + (rect.getHeight()/2));
+        c3.setCenterX(rect.getX() + x + (rect.getWidth()/2) + 15);
+        c3.setCenterY(rect.getY() + (rect.getHeight()/2) + 15);
+        c4.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 15);
+        c4.setCenterY(rect.getY() + (rect.getHeight()/2) - 15);
+        c5.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 15);
+        c5.setCenterY(rect.getY() + (rect.getHeight()/2));
+        c6.setCenterX(rect.getX() + x + (rect.getWidth()/2) - 15);
+        c6.setCenterY(rect.getY() + (rect.getHeight()/2) + 15);
+        for (Circle c : set){
+            c.setFill(Color.BLACK);
+            c.setRadius(5);
+        }
+        return set;
+    }
+
+    private ParallelTransition parallel(Rectangle rect, int x){
+        TranslateTransition translate = new TranslateTransition(Duration.millis(500));
+        translate.setByX(x);
+        translate.setDuration(Duration.millis(1000));
+        RotateTransition rotate = new RotateTransition();
+        rotate.setByAngle(360);
+        rotate.setCycleCount(2);
+        return new ParallelTransition(rect, translate, rotate);
+    }
+
+    private Rectangle makeDice(){
+        Rectangle rect = new Rectangle(20,500,50,50);
+        Stop[] stops = new Stop[] {new Stop(0,Color.BLUE), new Stop(1, Color.AQUAMARINE)};
+        LinearGradient linear = new LinearGradient(0,0,1,0,true, CycleMethod.NO_CYCLE, stops);
+        rect.setFill(linear);
+        rect.setStroke(Color.WHITE);
+        rect.setArcWidth(15);
+        rect.setArcHeight(15);
+        return rect;
+    }
+
+    private TranslateTransition moveBack(Rectangle rect, int x){
+        TranslateTransition back = new TranslateTransition();
+        back.setByX(-x);
+        back.setDuration(Duration.millis(10));
+        back.setNode(rect);
+        return back;
+    }
+
+    private Label makeBoard(){
+        Image board = new Image("monopoly.png");
+        ImageView boardView = new ImageView(board);
+        boardView.setFitHeight(800);
+        boardView.setFitWidth(1000);
+        Label lbl = new Label();
+        lbl.setGraphic(boardView);
+        lbl.setMaxHeight(800);
+        lbl.setMaxWidth(1000);
+        return lbl;
+    }
+
+    private ImageView figure(){
+        Image figure = new Image("figure.png");
+        ImageView figureView = new ImageView(figure);
+        figureView.setTranslateX(900);
+        figureView.setTranslateY(700);
+        return  figureView;
+    }
+}
