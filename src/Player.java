b3ae460d5886296b3295543ea615483098ffecca
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-11-30 09:58:39 +0100

    Added Player class with few functions (some of them will be used later)

---
+++ src/Player.java
@@ -0,0 +1,55 @@
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+
+public class Player {
+    private int account;
+    private int pos;
+    private ArrayList<String> owned;
+    private Image figure;
+
+    public Player(int num){
+        account = 500;
+        owned = null;
+        pos = num;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public void setAccount(int account) {
+        this.account = account;
+    }
+
+    public ArrayList<String> getOwned() {
+        return owned;
+    }
+
+    public void setOwned(ArrayList<String> owned) {
+        this.owned = owned;
+    }
+
+    public void addOwned(String name){
+        owned.add(name);
+    }
+
+    public void setFigure(String img){
+        figure = new Image(img);
+    }
+
+    public Image getFigure(){
+
+        return figure;
+    }
+
+    public boolean isPlaying(){
+        if (figure != null)
+            return true;
+        return false;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+}
