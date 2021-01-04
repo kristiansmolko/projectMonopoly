c14aca49e6e40384704d3987cff9653383d254f5
SamuelLenart <72248460+SamuelLenart@users.noreply.github.com>
2021-01-04 12:54:03 +0100

    Changed prices

--- src/Chance.java
+++ src/Chance.java
@@ -17,18 +17,19 @@ public class Chance {
         }
     }
 
+
     private PolicyParser.PermissionEntry card;
-    Chance getloseTax = new Chance("Your town's raising taxes, for your every house pay $250", "money, move", -250, card);
-    Chance getMoveToExtra = new Chance("Move to the nearest EXTRA square. When you pass through START pick up $2000", "move", 2000, card);
-    Chance getloseMoney150 = new Chance("You lost $150", "money", -150, card);
+    Chance getloseTax = new Chance("Your town's raising taxes, for your every house pay $50", "money, move", -50, card);
+    Chance getMoveToExtra = new Chance("Move to the nearest EXTRA square. When you pass through START pick up $200", "move", 200, card);
+    Chance getloseMoney150 = new Chance("You lost $100", "money", -100, card);
     Chance getMove3Squares = new Chance("Move 3 squares back!", "move", 3, card);
     Chance getwinMoney150 = new Chance("You won $150", "money", 150, card);
     Chance loseMoney50 = new Chance("You lost $50", "money", -50, card);
-    Chance getwinMoney350 = new Chance("You won $350", "money", 350, card);
-    Chance start = new Chance("Move to start and pick up $2000", "money, move", 2000, card);
-    Chance getMoveToPortal = new Chance("Move to the nearest EXTRA square. When no one owns this square u can buy it. When someone owns it u pay double of claim. When you pass through START pick up $2000", "move", 2000, card);
-    Chance getMoveToJail = new Chance("You got yourself to jail. Move to jail, you won't get through start and you won't get $2000", "move", 0, card);
-    Chance getMovetonearestSquare = new Chance("Move to the nearest square, when someone someone owns it roll the dice and pay 2 times more of the roll. When you pass through START pick up $2000", "move", 2000, card);
-    Chance goToThePigFarm = new Chance("Move to pig farm. When you pass through START pick up $2000", "move, money", 2000, card);
+    Chance getwinMoney350 = new Chance("You won $300", "money", 300, card);
+    Chance start = new Chance("Move to start and pick up $200", "money, move", 200, card);
+    Chance getMoveToPortal = new Chance("Move to the nearest EXTRA square. When no one owns this square u can buy it. When someone owns it u pay double of claim. When you pass through START pick up $200", "move", 200, card);
+    Chance getMoveToJail = new Chance("You got yourself to jail. Move to jail, you won't get through start and you won't get $200", "move", 0, card);
+    Chance getMovetonearestSquare = new Chance("Move to the nearest square, when someone someone owns it roll the dice and pay 2 times more of the roll. When you pass through START pick up $200", "move", 200, card);
+    Chance goToThePigFarm = new Chance("Move to pig farm. When you pass through START pick up $200", "move, money", 200, card);
     Chance getOutofJail = new Chance("Get out of jail free","prison",0, card);
 }
